057b4db296a85a1b9c28571cd8acd8522a1cf789
melalex <melalex490@gmail.com>
2017-04-18 13:04:22 +0300

    Frame

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,58 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static final double SCENE_WIDTH = 300;
+    private static final double SCENE_HEIGHT = 250;
+    private static final double MARGIN = 10;
+    private static final double BOT_MARGIN = 50;
+    private static final double LINE_WIDTH = 3;
+    private static final double BOT_WIDTH = SCENE_WIDTH - 2 * (MARGIN + BOT_MARGIN) + 2 * LINE_WIDTH;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Lab 1");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Group root = new Group();
+        Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+
+        scene.setFill(Color.GREEN);
+
+        final Line line1 = new Line(
+                MARGIN,
+                MARGIN,
+                BOT_MARGIN + MARGIN,
+                SCENE_HEIGHT - MARGIN
+        );
+        line1.setStroke(Color.RED);
+        line1.setStrokeWidth(LINE_WIDTH);
+        root.getChildren().add(line1);
+
+        final Line line2 = new Line(
+                BOT_MARGIN + MARGIN,
+                SCENE_HEIGHT - MARGIN,
+                BOT_WIDTH + BOT_MARGIN + MARGIN,
+                SCENE_HEIGHT - MARGIN
+        );
+        line2.setStroke(Color.RED);
+        line2.setStrokeWidth(LINE_WIDTH);
+        root.getChildren().add(line2);
+
+        final Line line3 = new Line(
+                BOT_WIDTH + BOT_MARGIN + MARGIN,
+                SCENE_HEIGHT - MARGIN,
+                BOT_WIDTH + 2 * BOT_MARGIN,
+                MARGIN
+        );
+        line3.setStroke(Color.RED);
+        line3.setStrokeWidth(LINE_WIDTH);
+        root.getChildren().add(line3);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
